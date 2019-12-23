ec7601278f5d700d213324e4823e63fea78bb685
szlin <2223012067@qq.com>
2019-12-23 10:42:42 +0800

    Add files via upload

---
+++ eureka/src/test/java/lsz/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package lsz.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
